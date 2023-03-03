fd8d58637328db7abf1a80286247836610d0578c
FreddyKly <Frederik.Kliemt@web.de>
2023-03-03 18:32:12 +0100

    responsive

--- Frontend/src/components/QuestionGrid.vue
+++ Frontend/src/components/QuestionGrid.vue
@@ -1,10 +1,10 @@
 <template>
   <div v-for="category in categories" :key=category.id class="column no-wrap">
-    <div class="row text-right text-uppercase text-h4">
+    <div class="row flex justify-center text-uppercase text-h4">
       {{ category.title }}
     </div>
     <div v-for="question in category.questions" :key="question.id" class="row no-wrap">
-        <q-card class="question bg-teal-3 q-ma-sm">
+        <q-card class="question bg-teal-3 q-ma-sm" v-bind:style="$q.screen.gt.md ? {'width' : '270px'} : {'width' : '200px'}">
           <q-card-section>
             <div class="text-black text-center text-h1 text-weight-bold">
               {{ question.points }}
@@ -41,7 +41,7 @@ export default defineComponent({
 </script>
 
 <style lang="sass" scoped>
-.question
-  width: 300px
-  max-width: 300px
-</style>
+// .question
+//   width: 300px
+//   max-width: 300px
+// </style>
