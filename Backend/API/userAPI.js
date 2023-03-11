36d3f9247d60e7a0544401397efafb44f2a183f2
Frederik Kliemt <Frederik.Kliemt@iteratec.com>
2023-03-11 20:09:52 +0100

    general API built

---
+++ Backend/API/userAPI.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require("mongoose")
+
+const gameModel = require("./Schemas/Game")
+const userModel = require("./Schemas/User")
+const question = require("./Schemas/Questions")
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    console.log('Post-Request for a User')
+    const user = await userModel.create({name: req.body.name})
+    res.status(201).json({userID: user._id})
+})
