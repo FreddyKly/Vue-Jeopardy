4cabd7f6ec5e4c91944d38bc92e2814e4c937577
FreddyKly <Frederik.Klie@gmail.com>
2023-03-11 20:11:41 +0100

    get game

--- Backend/API/gameAPI.js
+++ Backend/API/gameAPI.js
@@ -12,3 +12,13 @@ router.post('/', async (req, res) => {
     const game = await gameModel.create({creator: req.body.user})
     res.status(201).json({gameID: game._id})
 })
+
+router.get('/', async (req, res) => {
+    console.log('Get-Request for Game')
+    const game = gameModel.where("_id").equals(req.body.gameID)
+    const q1 = await questionColumnModel.create({
+        topic: req.body.topic,
+        questions: req.body.questions
+    })
+    game.questions.push(q1)
+})
