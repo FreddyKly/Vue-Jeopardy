18af582318e8c74425ddc28bd931f23e821374d7
FreddyKly <Frederik.Kliemt@web.de>
2023-03-14 18:41:05 +0100

    use route differently

--- Frontend/src/pages/IndexPage.vue
+++ Frontend/src/pages/IndexPage.vue
@@ -1,17 +1,23 @@
 <template>
     <q-page class="flex justify-center items-center">
-        <q-btn class="my-font bg-teal-3" v-on:click="$router.push('/Grid')" icon="add" label="New Game" text-color="black"/>
+        <q-btn class="my-font bg-teal-3" v-on:click="newGame()" icon="add" label="New Game" text-color="black"/>
     </q-page>
     
   </template>
   
   <script lang="ts">
   import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
   
   export default defineComponent({
     name: 'QuestionPage',
     setup () {
-      return {  };
+      const router = useRouter()
+      function newGame() {
+        console.log('hey')
+        router.push('/Grid')
+      }
+      return { newGame: newGame };
     }
   });
   </script>
