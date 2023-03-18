963dcdf418b646479b1b4edebae0923fba11639e
FreddyKly <Frederik.Kliemt@web.de>
2023-03-18 01:13:55 +0100

    test

--- Frontend/quasar.config.js
+++ Frontend/quasar.config.js
@@ -70,7 +70,7 @@ module.exports = configure(function (ctx) {
       publicPath: '/',
       // analyze: true,
       env: {
-        API: ctx.dev ? 'http://localhost:8080' : `${process.env.API}`
+        API: ctx.dev ? 'http://localhost:8080' : 'https://pastoral-office-production.up.railway.app/'
       },
       // rawDefine: {}
       // ignorePublicFolder: true,
