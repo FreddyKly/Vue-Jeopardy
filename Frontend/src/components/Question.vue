672c0c77ebaa079537e87cd93f6cece025be0bfe
FreddyKly <Frederik.Kliemt@web.de>
2023-03-18 14:44:49 +0100

    styling for Questionpage and edit button

--- Frontend/src/components/Question.vue
+++ Frontend/src/components/Question.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="text-black text-h3" id="topic">
     {{ topic }}
+    <p class="q-pl-lg text-h6 ">{{ ((Number($route.params.Qid) + 1) % 5) * 100 }}</p>
   </div>
-  <div class="column full-width full-height flex justify-center items-center">
-    <q-input borderless type="textarea" v-on:blur="questionChanged()" autogrow v-model.trim="question"
+  <div v-if="isEdited" class="col q-ma-lg flex justify-end items-start">
+    <q-btn color="teal-5" icon="edit" v-on:click="isEdited = false">
+    </q-btn>
+  </div>
+
+  <div v-if="!isEdited" class="column full-width full-height flex justify-center items-center">
+    <q-input borderless type="textarea" v-on:blur="questionChanged()" autogrow v-model.trim="questionText"
       :input-style="{ fontSize: '45px', width: '400px', lineHeight: '45px', textAlign: 'center' }" />
-    <h6>or</h6>
+    <h6 >or</h6>
     <q-uploader url="http://localhost:4444/upload" style="max-width: 300px" label="Upload Files" color="teal-4"
       text-color="black" class="bg-teal-2" />
   </div>
+  <div v-if="isEdited" class="column full-width full-height flex justify-center items-center">
+    <p :style="{ fontSize: '45px', width: '400px', lineHeight: '45px', textAlign: 'center' }">{{ questionText }}</p>
+  </div>
+
   <q-page-sticky position="bottom-left" :offset="[18, 18]">
     <q-btn fab icon="arrow_back" color="teal-5" v-on:click="$router.push({ path: `/${$route.params.GameID}` })" />
   </q-page-sticky>
@@ -25,34 +35,38 @@ import { api } from 'src/boot/axios';
 export default defineComponent({
   name: 'QuestionComponent',
   async setup() {
-    var question = ref<string>()
     const route = useRoute()
+    var questionText = ref<string>()
+    const questionIdx = Number(route.params.Qid) % 5
+    const columnIdx = Math.trunc(Number(route.params.Qid) / 5)
+    
     const gameJson = JSON.stringify({ gameID: `${route.params.GameID}` });
     const resGame = await api.post('/api/game', gameJson, {
       headers: {
         'Content-Type': 'application/json'
       }
     })
-     
-    const column = Math.trunc(Number(route.params.Qid) / 5)
-    console.log(resGame.data.categories[column].questions[Number(route.params.Qid)])
-    const topic = resGame.data.categories[column].topic
-    question.value = resGame.data.categories[column].questions[(Number(route.params.Qid) % 5)].question
+    
+    const category = resGame.data.categories[columnIdx]
+    const topic = category.topic
+    var isEdited = ref<boolean>(category.questions[questionIdx].edited)
+    questionText.value = category.questions[questionIdx].question
+    console.log(isEdited)
 
     async function questionChanged() {
         const questionJson = JSON.stringify({ 
           gameID: `${route.params.GameID}`,
           questionID: route.params.Qid,
-          question: question.value,
+          question: questionText.value,
           edited: true
         });
-        const resTopic = await api.post('/api/game/question', questionJson, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
+        await api.post('/api/game/question', questionJson, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
       })
     }
-    return { topic, question, questionChanged };
+    return { topic, questionText, isEdited, questionChanged };
   },
 });
 </script>
